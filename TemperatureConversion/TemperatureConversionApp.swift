1a07daf29a34feb65fe089e82905e5f2fd55d496
Dwiki <dwikis17@gmail.com>
2023-02-26 16:05:27 +0700

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Dwiki on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
