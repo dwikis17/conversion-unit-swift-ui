9ee7ce4e3f9e54b3f846098ec53cca2746266057
Dwiki <dwikis17@gmail.com>
2023-02-26 16:34:32 +0700

    temperature conversion

--- TemperatureConversion/ContentView.swift
+++ TemperatureConversion/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var temperatureValue = 0.0
+    @State private var selectedUnit = "Kelvin"
+    @FocusState private var isTemperatureValueFocused:Bool
+    
+    var convertedValue : Double{
+        
+        if(selectedUnit == "Kelvin"){
+            return temperatureValue + 273.15
+        } else if (selectedUnit == "Reamur"){
+            return temperatureValue * 4/5
+        } else {
+            return (temperatureValue * 9/5) + 32
         }
-        .padding()
+    }
+    
+    let temperatureUnits = ["Kelvin", "Reamur", "Fahrenheit"]
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Value:" , value: $temperatureValue, format: .number )
+                        .keyboardType(.decimalPad)
+                        .focused($isTemperatureValueFocused)
+                } header: {
+                    Text("Input value in celsius")
+                }
+                
+                Section{
+                    Picker("Select Unit", selection: $selectedUnit){
+                        ForEach(temperatureUnits, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select unit")
+                }
+                
+                Section{
+                    Text(convertedValue, format:.number)
+                } header: {
+                    Text("Value in \(selectedUnit)")
+                }
+            }
+            .navigationTitle("Conversion Unit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button("Done"){
+                        isTemperatureValueFocused = false
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
